8c9e5a3f12cb396343d62d47d073d80f9a47caac
Rosalie AGUILLON <raguillo@42.student.fr>
2022-10-14 15:38:30 +0200

    fix tests

--- test/bool.ml
+++ test/bool.ml
@@ -1,36 +1,36 @@
 open Ocamlib
 
 let test_not () =
-  Alcotest.(check bool) false (not true)
-  Alcotest.(check bool) true (not false)
+  Alcotest.(check bool) "same bool" false (not true);
+  Alcotest.(check bool) "same bool" true (not false)
 
 let test_(&&) () =
-  Alcotest.(check bool) false ((&&) false false)
-  Alcotest.(check bool) false ((&&) false true)
-  Alcotest.(check bool) false ((&&) true false)
-  Alcotest.(check bool) true ((&&) true true)
+  Alcotest.(check bool) "same bool" false ((&&) false false);
+  Alcotest.(check bool) "same bool" false ((&&) false true);
+  Alcotest.(check bool) "same bool" false ((&&) true false);
+  Alcotest.(check bool) "same bool" true ((&&) true true)
 
 let test_(||) () =
-  Alcotest.(check bool) false ((||) false false)
-  Alcotest.(check bool) true ((||) false true)
-  Alcotest.(check bool) true ((||) true false)
-  Alcotest.(check bool) true ((||) true true)
+  Alcotest.(check bool) "same bool" false ((||) false false);
+  Alcotest.(check bool) "same bool" true ((||) false true);
+  Alcotest.(check bool) "same bool" true ((||) true false);
+  Alcotest.(check bool) "same bool" true ((||) true true)
 
 let test_equal () =
-  Alcotest.(check bool) true (equal false false)
-  Alcotest.(check bool) true (equal true true)
-  Alcotest.(check bool) false (equal true false)
-  Alcotest.(check bool) false (equal false true)
+  Alcotest.(check bool) "same bool" true (equal false false);
+  Alcotest.(check bool) "same bool" true (equal true true);
+  Alcotest.(check bool) "same bool" false (equal true false);
+  Alcotest.(check bool) "same bool" false (equal false true)
 
 let test_to_int () =
-  Alcotest.(check int) 0 (to_int false)
-  Alcotest.(check int) 1 (to_int true)
+  Alcotest.(check int) "same int" 0 (to_int false);
+  Alcotest.(check int) "same int" 1 (to_int true)
 
 let test_to_float () =
-  Alcotest.(check float) 0. (to_float false)
-  Alcotest.(check float) 1. (to_float true)
+  Alcotest.(check float) "same float" 0. (to_float false);
+  Alcotest.(check float) "same float" 1. (to_float true)
 
 let test_to_string () =
-  Alcotest.(check string) "false" (to_string false)
-  Alcotest.(check string) "true" (to_string true)
+  Alcotest.(check string) "same string" "false" (to_string false);
+  Alcotest.(check string) "same string" "true" (to_string true)
 
--- test/unit.ml
+++ test/unit.ml
@@ -1,10 +1,10 @@
 open Ocamlib
 
 let test_equal () =
-  Alcotest.(check bool) true (equal () ())
+  Alcotest.(check bool) "same bool" true (equal () ())
 
 let test_compare () =
-  Alcotest.(check int) 0 (compare () ())
+  Alcotest.(check int) "same int" 0 (compare () ())
 
 let test_to_string () =
-  Alcotest.(check string) "()" (to_string ())
+  Alcotest.(check string) "same string" "()" (to_string ())
